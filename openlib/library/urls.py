14375d96b4f032048d1900ad2c0998275e8d0a9d
faisal_azhar <faisalazhar.itu@gmail.com>
2022-12-15 19:41:22 +0600

    Connected database mongodb & oracle

---
+++ openlib/library/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from library.views import index
+
+app_name = "library"
+
+urlpatterns = [
+    path('', index, name="index"),
+]
---
+++ openlib/routers/db_routers.py
@@ -0,0 +1,78 @@
+class AuthRouter:
+    """
+    A router to control all database operations on models in the
+    auth and contenttypes applications.
+    """
+    route_app_labels = {'auth', 'contenttypes', 'admin', 'sessions'}
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'oracle'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'oracle'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the auth or contenttypes apps is
+        involved.
+        """
+        if (
+            obj1._meta.app_label in self.route_app_labels or
+            obj2._meta.app_label in self.route_app_labels
+        ):
+           return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == 'oracle'
+        return None
+    
+class MongoDBRouter:
+    """
+    A router to control all blogs and chats.
+    """
+    route_app_labels = {'blog'}
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'mongodb'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'mongodb'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the auth or contenttypes apps is
+        involved.
+        """
+        if (
+            obj1._meta.app_label in self.route_app_labels or
+            obj2._meta.app_label in self.route_app_labels
+        ):
+           return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == 'mongodb'
+        return None
